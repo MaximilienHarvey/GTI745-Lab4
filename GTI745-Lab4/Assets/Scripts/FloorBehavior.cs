1777ddf5ea3fd42325fea5c34b031fc166739f75
Leah Fortin <skaryu64@gmail.com>
2025-03-23 15:18:09 -0400

    Floor collider to make the luggage respawn when it falls

---
+++ GTI745-Lab4/Assets/Scripts/FloorBehavior.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorBehavior : MonoBehaviour
+{
+    public Transform respawnPos;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Luggage"))
+        {
+            collision.transform.position = respawnPos.position;
+            collision.transform.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
